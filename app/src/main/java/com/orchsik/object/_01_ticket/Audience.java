6c5417a265c8dcd2c16d64b6ad5d9ca2fe8b31af
orchsik <orchsik@naver.com>
2022-12-06 07:17:25 +0900

    Add 관람객

---
+++ app/src/main/java/com/orchsik/object/_01_ticket/Audience.java
@@ -0,0 +1,14 @@
+package com.orchsik.object._01_ticket;
+
+public class Audience {
+  private Bag bag;
+
+  public Audience(Bag bag) {
+    this.bag = bag;
+  }
+
+  public Bag getBag() {
+    return bag;
+  }
+
+}
