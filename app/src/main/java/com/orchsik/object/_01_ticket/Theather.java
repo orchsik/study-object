24ef6f31d53ed8da73875b8e3b4ec8337bb1b060
orchsik <orchsik@naver.com>
2022-12-06 07:18:24 +0900

    Add 극장

---
+++ app/src/main/java/com/orchsik/object/_01_ticket/Theather.java
@@ -0,0 +1,21 @@
+package com.orchsik.object._01_ticket;
+
+public class Theather {
+  private TicketSeller ticketSeller;
+
+  public Theather(TicketSeller ticketSeller) {
+    this.ticketSeller = ticketSeller;
+  }
+
+  public void enter(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().setTicket(ticket);
+    } else {
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().minusAmount(ticket.getFee());
+      ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
+  }
+}
