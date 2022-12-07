ab4db40f8c3427e9850c86cfbc96c3720770a357
orchsik <orchsik@naver.com>
2022-12-07 13:01:01 +0900

    Refactor: 매표표표소 자유롭게 해주자(결합도 낮추기)

--- app/src/main/java/com/orchsik/object/_01_ticket/TicketSeller.java
+++ app/src/main/java/com/orchsik/object/_01_ticket/TicketSeller.java
@@ -8,8 +8,7 @@ public class TicketSeller {
   }
 
   public void sellTo(Audience audience) {
-    ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
-
+    ticketOffice.sellTicketTo(audience);
   }
 
 }
