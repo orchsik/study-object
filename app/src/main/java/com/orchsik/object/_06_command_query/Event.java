20d9a661c1abf6ef60d1c49af9152c05cc481134
orchsik <orchsik@naver.com>
2022-12-13 23:17:42 +0900

    Add: query & command 분리

--- app/src/main/java/com/orchsik/object/_06_command_query/Event.java
+++ app/src/main/java/com/orchsik/object/_06_command_query/Event.java
@@ -14,17 +14,22 @@ public class Event {
     this.duration = duration;
   }
 
+  /**
+   * 버그가 발생할 수 있음.
+   * 명령과 쿼리의 두 가지 역할을 동시에 수행하고 있기 때문에.
+   * Event가 RecurringSchedule에 설정된 조건을 만족하지 못할 경우 Event의 상태를 조건을
+   * 만족시키도록 변경한 후(여기가 문제다!) false를 반환한다.
+   */
   public boolean isSatisfied(RecurringSchedule schedule) {
     if (from.getDayOfWeek() != schedule.getDayOfWeek()
         || !from.toLocalTime().equals(schedule.getFrom())
         || !duration.equals(schedule.getDuration())) {
       reschedule(schedule);
-      return false;
     }
     return true;
   }
 
-  private void reschedule(RecurringSchedule schedule) {
+  public void reschedule(RecurringSchedule schedule) {
     from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)), schedule.getFrom());
     duration = schedule.getDuration();
   }
