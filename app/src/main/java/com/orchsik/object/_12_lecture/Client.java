9861418000ae64eca16ac97b619bbadbb7032815
orchsik <orchsik@naver.com>
2022-12-25 09:14:29 +0900

    Add: GradeLecture (코드재사용을 위한 상속)

--- app/src/main/java/com/orchsik/object/_12_lecture/Client.java
+++ app/src/main/java/com/orchsik/object/_12_lecture/Client.java
@@ -10,5 +10,16 @@ public class Client {
      */
     Lecture lecture = new Lecture("객체지향 프로그래밍", 70, Arrays.asList(81, 95, 75, 50, 45));
     String evaluation = lecture.evaluate(); // 결과 => "Pass:3 Fail:2"
+
+    Lecture gradeLecture = new GradeLecture(
+        "객체지향 프로그래밍",
+        70,
+        Arrays.asList(81, 95, 75, 50, 45),
+        Arrays.asList(
+            new Grade("A", 100, 95),
+            new Grade("B", 94, 80),
+            new Grade("C", 79, 70),
+            new Grade("D", 69, 50),
+            new Grade("F", 49, 0)));
   }
 }
